be784d105efdfd77da09a9ba2bd71b995800aa7f
Chukxz <109679877+Chukxz@users.noreply.github.com>
2023-04-05 15:22:29 +0100

    Update Test_One.cpp
    Commented out unnecessary information.

--- Test_Folder/Test_One/Test_One.cpp
+++ Test_Folder/Test_One/Test_One.cpp
@@ -1,4 +1,4 @@
-Un#include <iostream>
+#include <iostream>
 using namespace std;
 
 //Palindrome test program v1.0
@@ -33,15 +33,15 @@ int recurse(int a,int no){
     int res;
     int result;
     if(no == 0){
-        #cout<<a<<endl;
+        //cout<<a<<endl;
         return a;
     }
     else{
-        #cout<<a<<endl;
+        //cout<<a<<endl;
         res=a%10;
         result = (a - res)/10;
         a = result;
-        #cout<<" "<<res<<" "<<result<<" "<<no<<endl;
+        //cout<<" "<<res<<" "<<result<<" "<<no<<endl;
     return (res * powbase(10,no)) + recurse(a,no-1);
     }
 }
